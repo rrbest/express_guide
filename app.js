572973ecc1f7fa07781f6230f1b9ea3c2d48eefc
Ryan Olsen <ryano1110@gmail.com>
2017-05-04 17:35:18 -0700

    added ark-api code

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var app = express();
 
+var square = require('./square');
+//var arkAPI = require('./node_modules/ark-api/src/account.js')
+
+var request = require('request');
+var options = require('./node_modules/ark-api/src/options.js');
+var Api = require('./node_modules/ark-api/src/api.js');
+
+var Account = {};
+Account.getBalance = function(address, callback) {
+  Api.get({
+    url: options.url + '/api/accounts/getBalance',
+    qs: {
+      'address': address
+    },
+    json: true
+  }, callback);
+};
+
+
+
 app.get('/', function (req, res){
-	res.send('Hello World!');
+	res.send('Hello World!' + 
+		square.area(4) + 
+		Account.getBalance("AT2k1MydfeAnuHchJc3Z2EgcQPQoH4s5om",
+				  function(error, success, response) {
+				    console.log(response);
+				})
+		);
 });
 
 app.listen(3000, function(){
